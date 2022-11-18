ca18df07a4203d894bf5234410a04c219fb09386
Erick Gonzalez Gonzalez <gonhoff@hotmail.com>
2022-11-18 16:27:53 +0100

    hello

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,21 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  #HACER ABAJO DE ESTA LINEA
+
+  get "/", to: "pages#home"
+
 end
+
+
+#home page HACER LAS RUTAS
+
+#controllador terminal #
+
+#controller que se va a llamar PAGES > plural
+
+#methodo o accion que se llama HOME # luego del controller
+
+#crear el view manualmente HOME dentro de PAGES
+
+# Dentro del archivo html , escribit h1 - My primera pagina
